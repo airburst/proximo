9d9757d197a0bfe77757a5108171ad43ffbf60ba
airburst <mark@fairhursts.net>
2016-07-12 20:02:22 +0100

    Removed some mock data

--- app/mock.ts
+++ app/mock.ts
@@ -3,16 +3,23 @@ export const MockPoints = [
         name: 'Lysa',
         location: { lng: -2.7016700, lat: 51.1417600 },
         color: 'red'
-    },
-    {
-        name: 'Eastbourne',
-        location: { lng: 0.2988500, lat: 50.7728600 },
-        color: 'green'
-    },
-    {
-        name: 'Rejerrah',
-        location: { lng: -5.0960300, lat: 50.3621100 },
-        color: 'black'
     }
 ];
 
+// export const MockPoints = [
+//     {
+//         name: 'Lysa',
+//         location: { lng: -2.7016700, lat: 51.1417600 },
+//         color: 'red'
+//     },
+//     {
+//         name: 'Eastbourne',
+//         location: { lng: 0.2988500, lat: 50.7728600 },
+//         color: 'green'
+//     },
+//     {
+//         name: 'Rejerrah',
+//         location: { lng: -5.0960300, lat: 50.3621100 },
+//         color: 'black'
+//     }
+// ];
