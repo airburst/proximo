3dbf4d1d323ea9ae647785e4102b1a2288377f97
airburst <mark@fairhursts.net>
2016-07-18 08:17:09 +0100

    Added distance to googlemap

--- app/googlemap.ts
+++ app/googlemap.ts
@@ -81,4 +81,28 @@ export class GoogleMap {
         this.map.fitBounds(this.bounds);
     }
 
+    public distanceBetween(point1: Marker, point2: Marker): number {
+        let d1 = window.google.maps.geometry.spherical.computeDistanceBetween(point1.location, point2.location),
+            d2 = window.google.maps.geometry.spherical.computeDistanceBetween(point2.location, point1.location);
+        return Math.min(d1, d2);
+    }
+
+    // function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    //     var R = 6371; // Radius of the earth in km
+    //     var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    //     var dLon = deg2rad(lon2 - lon1);
+    //     var a =
+    //         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    //         Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    //         Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    //         ;
+    //     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    //     var d = R * c; // Distance in km
+    //     return d;
+    // }
+
+    // function deg2rad(deg) {
+    //     return deg * (Math.PI / 180)
+    // }
+
 }
